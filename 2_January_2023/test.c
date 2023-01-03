170eb560505f2cbc854f86491ea18c59fb4463f1
windowsofshy <872761393@qq.com>
2023-01-04 00:43:55 +0800

    1:42 a.m

---
+++ 2_January_2023/test.c
@@ -0,0 +1,5 @@
+/* a test */
+#include <stdio.h>
+int main(){
+    printf ("My first program.");
+}
---
+++ 3_January_2023/test.c
@@ -0,0 +1,8 @@
+/*Write a program to print your name and address.*/
+#include<stdio.h>
+int main (){
+    char name[10] = "Jove";
+    char address[20] = "4114Sepulveda Blvd";
+    printf("Print your name and address:%s,%s",name,address);
+    return 0;
+}
