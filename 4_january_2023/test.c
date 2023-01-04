232426269690e8b112a4805336ce5bb062950add
windowsofshy <872761393@qq.com>
2023-01-04 21:18:32 +0800

    21:17 pm

---
+++ 4_january_2023/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*function declaration*/
+void  jolly();
+void deny();
+int main (){
+    jolly();
+    jolly();
+    jolly();
+    deny();
+
+}
+void jolly(){
+    printf("For he's a jolly good  fellow!\n");
+}
+void deny(){
+    printf("Which nobody  can deny!");
+}
