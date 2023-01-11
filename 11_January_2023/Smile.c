ca40e0d5e1149f468a240cd010b91699c09ea9a4
windowsofshy <872761393@qq.com>
2023-01-12 00:59:53 +0800

    1:00 a.m

---
+++ 11_January_2023/Smile.c
@@ -0,0 +1,20 @@
+/*  Write a program to generate the fellowing output:
+    Smile!Smile!Smile!
+    Smile!Smile!
+    Smile!
+*/
+#include <stdio.h>
+void Smile () ;
+int main() {
+    Smile();
+    Smile();
+    Smile();
+    printf("\n");
+    Smile();
+    Smile();
+    printf("\n");
+    Smile();
+}
+void Smile () {
+    printf("Smile!");
+}
